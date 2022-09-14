361efc9b6c6bbbc35a79ea74a93066fca86acc30
xieweixing <xwx@addcn.com>
2022-09-14 11:09:35 +0800

    modify spiders

--- spiders/leslie.py
+++ spiders/leslie.py
@@ -1,20 +1,19 @@
-# -*- utf-8 -*-
-import requests
+import os
+
 from lxml import etree
 import requests.packages.urllib3
 
 requests.packages.urllib3.disable_warnings()
-
 url = "https://movie.douban.com/celebrity/1003494/photos/?type=C&start=%s&sortby=like&size=a&subtype=a"
 headers = {
-    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
-    'referer':'https://movie.douban.com/celebrity/1003494/photos/?type=C&start=%s&sortby=like&size=a&subtype=a'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+    'referer': 'https://movie.douban.com/celebrity/1003494/photos/?type=C&start=%s&sortby=like&size=a&subtype=a'
 }
 for i in range(20):
-    print('第%d页' % i)
+    print(i)
     html = etree.HTML(requests.get(url % (i * 30), headers=headers, verify=False).content.decode())
     img_links = html.xpath('//*[@class="cover"]/a/img/@src')
     for img_link in img_links:
         body = requests.get(img_link, headers=headers).content
-        with open('./bg/' + img_link.split('/')[-1].split('.')[0] + '.jpg', 'wb') as f:
+        with open(os.path.abspath(os.path.dirname(__file__)) + '.././bg/' + img_link.split('/')[-1].split('.')[0] + '.jpg', 'wb') as f:
             f.write(body)
