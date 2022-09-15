cafcb0762f0187e30e4a7be22bde7f7b2bca0775
makubex <1105504520@qq.com>
2022-09-15 17:52:33 +0800

    init flask

--- web/app.py
+++ web/app.py
@@ -3,6 +3,18 @@ import waitress
 from flask import Flask, request, send_from_directory, render_template
 from flask_cors import CORS, cross_origin
 
+app = Flask(__name__)
+cors = CORS(app)
+
+
 @app.route('/', methods=['GET'])
 def home():
     return "hello world"
+
+
+@app.route('/uploads', methods=['POST'])
+def upload():
+    return "upload"
+
+if __name__ == '__main__':
+    waitress.serve(app, host='0.0.0.0', port=5000)
